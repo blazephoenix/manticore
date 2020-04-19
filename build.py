9c87f89f41571d9f7a3f8535b479c69e95127e0c
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2020-04-19 18:04:56 +0530

    added cli, refactoring

--- build.py
+++ build.py
@@ -1,11 +1,30 @@
-import render, parse
+import parse
 import logging
+import click
+from render import Render
 
 logging.basicConfig(level = logging.INFO)
 
+@click.command()
+@click.option('--default', '-d', help='Generate the default blog template')
+@click.option('--resume','-r', help='Generate a resume template')
+
+def build(default, resume):
+    Renderer = Render()
+    if default:
+        try:
+            posts = parse.Post_parser()
+            Renderer.Render_posts(posts)
+            logging.info(" Build successful. Check your output folder.")
+        except:
+            logging.exception(" Build failed :(")           
+    elif resume:
+        try:
+            details = parse.Resume_parser()
+            Renderer.Render_resume(details)
+            logging.info(" Build successful. Check your output folder.")
+        except:
+            logging.exception(" Build failed :(")
+
 if __name__ == "__main__":
-    try:
-        render.Render(parse.Parser())
-        logging.info(" Build successful. Check your output folder.")
-    except:
-        logging.exception(" Build failed :(")
+    build()
--- render.py
+++ render.py
@@ -1,51 +1,74 @@
 import os
 from pathlib import Path
 from jinja2 import Environment, PackageLoader
-from parse import Parser
+from parse import Resume_parser, Post_parser
 
-def Render(posts):
-
-    POSTS = posts
+class Render:
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
     env = Environment(loader=PackageLoader('render', 'templates'))
-    home_temp = env.get_template('default/home.html')
-    post_temp = env.get_template('default/post.html')
 
-    posts_mdata = [POSTS[post].metadata for post in POSTS]
-    # tags = [post['tags'] for post in posts_mdata]
-    # print(posts_mdata)
-    home_html = home_temp.render(posts=posts_mdata)
+    def Render_posts(self, posts):
+            POSTS = posts
 
-    if Path(os.path.join(ROOT_DIR, 'output', 'home.html')).is_file():
-        with open('output/home.html', 'w') as file:
-            file.write(env.get_template('home.html').render(posts=posts_mdata))
-    else:
-        os.makedirs(os.path.join(ROOT_DIR, 'output'))
+            home_temp = self.env.get_template('default/home.html')
+            post_temp = self.env.get_template('default/post.html')
 
-        with open('output/home.html', 'w') as file:
-            file.write(home_html)
+            posts_mdata = [POSTS[post].metadata for post in POSTS]
+            home_html = home_temp.render(posts=posts_mdata)
 
-    #FOR INDIVIDUAL POSTS///////////////////////////////////////
-    for post in POSTS:
-        post_metadata = POSTS[post].metadata
+            if Path(os.path.join(self.ROOT_DIR, 'output', 'home.html')).is_file():
+                with open('output/home.html', 'w') as file:
+                    file.write(self.env.get_template('home.html').render(posts=posts_mdata))
+            else:
+                os.makedirs(os.path.join(self.ROOT_DIR, 'output'))
 
-        post_data = {
-            'blog': post_metadata['blog'],
-            'content': POSTS[post],
-            'title': post_metadata['title'],
-            'date': post_metadata['date'],
-        }
+                with open('output/home.html', 'w') as file:
+                    file.write(home_html)
 
-        post_html = post_temp.render(post=post_data)
 
-        post_file_path = 'output/posts/{slug}.html'.format(slug=post_metadata['slug'])
+            for post in POSTS:
+                post_metadata = POSTS[post].metadata
 
-        if Path(os.path.join(ROOT_DIR, post_file_path)).is_file():
-            with open(post_file_path, 'w') as file:
-                file.write(env.get_template('post.html').render(post=post_data))
-        else:
-            os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
-            
-            with open(post_file_path, 'w') as file:
-                file.write(post_html)
+                post_data = {
+                    'blog': post_metadata['blog'],
+                    'content': POSTS[post],
+                    'title': post_metadata['title'],
+                    'date': post_metadata['date'],
+                }
+
+                post_html = post_temp.render(post=post_data)
+
+                post_file_path = 'output/posts/{slug}.html'.format(slug=post_metadata['slug'])
+
+                if Path(os.path.join(self.ROOT_DIR, post_file_path)).is_file():
+                    with open(post_file_path, 'w') as file:
+                        file.write(self.env.get_template('post.html').render(post=post_data))
+                else:
+                    os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
+                    
+                    with open(post_file_path, 'w') as file:
+                        file.write(post_html)
+    
+    def Render_resume(self, details):    
+            DETAILS = details
+
+            resume_temp = self.env.get_template('resume/home.html')
+
+            resume_data = {
+                'name': details.metadata['name'],
+                'address': details.metadata['address'],
+                'phone': details.metadata['phone'],
+                'email':details.metadata['email'],
+                'content': details
+            }
+
+            resume_html = resume_temp.render(details=resume_data)
+
+            if Path(os.path.join(self.ROOT_DIR, 'output', 'resume.html')).is_file():
+                with open('output/resume.html', 'w') as file:
+                    file.write(self.env.get_template('resume/home.html').render(details=resume_data))
+            else:
+                os.makedirs(os.path.join(self.ROOT_DIR, 'output'))
+
+                with open('output/resume.html', 'w') as file:
+                    file.write(resume_html)
